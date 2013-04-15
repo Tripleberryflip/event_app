e698a5a8cf00327fff28d08fd83aaafacb60e223
Joseph Budin <jpbudi11@smumn.edu>
2013-04-15 00:17:55 -0500

    The change in these files came from working with my co-worker (Dylan) on a problem, therefore modifying files that did not need to be changed by me in the end. The User Stories that apply to this commit are, User Story: 6 and User Story: 7

--- app/controllers/judgesessions_controller.rb
+++ app/controllers/judgesessions_controller.rb
@@ -3,15 +3,12 @@ class JudgesessionsController < ApplicationController
     end
 
     def create
-
       judge = Judge.find_by_name(params[:judgesession][:name])
           if judge && judge.authenticate(params[:judgesession][:password])
             judge_sign_in judge
             redirect_to judge
           else
-
-            flash.now[:error] = 'Invalid user-name/password combination'
-
+            flash.now[:error] = 'Invalid user-name/password combination' # Not quite right!
             render 'new'
           end
     end
