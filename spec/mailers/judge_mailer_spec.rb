02f3787858260b5ff8546b739229215bed5e1729
Joseph Budin <jpbudi11@smumn.edu>
2013-04-16 21:45:01 -0500

    Adding tests to make sure that the mailer is working propoerly to notify judges that they have been created User Story: 21

--- spec/mailers/judge_mailer_spec.rb
+++ spec/mailers/judge_mailer_spec.rb
@@ -1,5 +1,15 @@
-require "spec_helper"
 
-describe JudgeMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+class JudgeMailerTest < ActionMailer::TestCase
+  def test_newjudge_email
+      judge = Judge.create!(:name => "Judge Judy", email: "user@example.com", password: "111111", password_confirmation: "111111")
+
+      # Send the email, then test that it got queued
+      email = UserMailer.newjudge_email(judge).deliver
+      assert !ActionMailer::Base.deliveries.empty?
+
+      # Test the body of the sent email contains what we expect it to
+      assert_equal [judge.email], email.to
+      assert_equal "You have been selected to be a Judge!", email.subject
+    end
 end
