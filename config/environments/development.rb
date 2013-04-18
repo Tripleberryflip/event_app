88f6e5073c1bdf346dd99bf9273b8b3423cf36e8
Joseph Budin <jpbudi11@smumn.edu>
2013-04-18 17:06:14 -0500

    Working on the mailer so that it we could debug it and make it not fail silently to find out what the problem it. Found the smtp settings, and changed them so that it is not conflicting with the creation of the judges User Story: 21

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,7 @@ EventApp::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
