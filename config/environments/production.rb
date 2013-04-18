78867c3b638fc79a50278d36ba990238bffe656d
Joseph Budin <jpbudi11@smumn.edu>
2013-04-17 19:19:34 -0500

    Modifications to attempt to get the mailer working properly. Making sure that the configuration files all have the right information in them. User Story: 21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ EventApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
