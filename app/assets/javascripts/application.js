76459207ee05fff044f90d120ca581bd3f8772a3
Dylan Ethen <dlethe10@smumn.edu>
2013-04-25 01:15:17 -0500

    Javascript dynamic nested form creation
    Implementation of javascript so that the score template form can have
    nested question forms in it that can be dynamically added and removed
    for a more functional and user friendly experience when creating a score
    template for an event (User story #8)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require jquery_ujs
 //= require_tree .
 //= require jquery.mobile
+
+
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
+}
